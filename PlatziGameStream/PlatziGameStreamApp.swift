3ed64f2a98d268e04bd2dad5c27ae7849ba25027
Martindavilacervantes <aldntmi@gmail.com>
2021-08-02 20:44:34 -0500

    SwiftUI-gameStremer

---
+++ PlatziGameStream/PlatziGameStreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlatziGameStreamApp.swift
+//  PlatziGameStream
+//
+//  Created by Martin Davila on 12/05/21.
+//
+
+import SwiftUI
+
+@main
+struct PlatziGameStreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
